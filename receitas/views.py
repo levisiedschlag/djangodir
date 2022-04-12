d4838db2abab86799f99579366d2b4a6ddd426cf
levisatto <levisiedschlag@gmail.com>
2022-04-12 17:37:36 -0300

    html

--- receitas/views.py
+++ receitas/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'receitas/home.html')
+    return render(request, 'receitas/pages/home.html')
 
 def sobre(request):
-    return render(request, 'receitas/home.html')
+    return render(request, 'receitas/pages/sobre.html')
 
 def contato(request):
     return HttpResponse('Contato')
