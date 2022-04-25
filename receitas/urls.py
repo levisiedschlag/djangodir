d870d61d82e07ad3b49320e3c8d329eb8899b2fb
levisatto <levisiedschlag@gmail.com>
2022-04-25 11:01:21 -0300

    empty recipes

--- receitas/urls.py
+++ receitas/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='recipes-home'),
+    path('', views.home, name='recipes-home'),
     path('recipes/category/<int:id>/', views.category, name='recipes-category'),
     path('recipes/<int:id>/', views.recipe, name='recipes-recipe'),
     
--- receitas/views.py
+++ receitas/views.py
@@ -1,22 +1,22 @@
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, render, get_object_or_404
 from utils.recipes import factory
 from receitas import models
 
 # Create your views here.
-def index(request):
-    recipes = models.Recipe.objects.filter(is_published=True).order_by('-id')
+def home(request):
+    recipes =models.Recipe.objects.filter(is_published=True,).order_by('-id')
     
     return render(request, 'receitas/pages/home.html', { 'recipes' : recipes })
 
 def category(request, id):
-    recipes = get_list_or_404(
-        models.Recipe.objects.filter(category__id=id, is_published=True).order_by('-id')
-        )
+    recipes = get_list_or_404(models.Recipe.objects.filter(category__id=id, is_published=True).order_by('-id'))
     return render(request, 'receitas/pages/recipe-category.html', { 
             'recipes' : recipes,
             'title': f'{recipes[0].category.name}'
             })
 
 def recipe(request, id):
-    recipe = models.Recipe.objects.filter(id=id, is_published=True).first()
+    # recipe = models.Recipe.objects.filter(id=id, is_published=True).first()
+
+    recipe = get_object_or_404(models.Recipe, id=id, is_published=True)
     return render(request, 'receitas/pages/recipe-view.html', { 'recipe':recipe, 'is_detail_page': True })
